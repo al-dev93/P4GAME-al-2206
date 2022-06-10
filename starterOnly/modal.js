917d56a36a2e0f2213afea62099e9e59fb4ebbff
A_Larose <larose.alain@gmail.com>
2022-06-10 02:43:07 +0200

    P4GAME-close_modal
    Fermeture de la fenêtre modale contenant le formulaire suite au click sur X, animation de la fermeture

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,32 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBackground = document.querySelector(".bground");
+const buttonRegistration = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+// TODO #201 cibler le control de fermeture de la modale
+const modalClosure = document.querySelector(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// modal event
+buttonRegistration.forEach((btn) => btn.addEventListener("click", commandModal));
+// TODO #202 évènement déclenchant la fermeture de la modale
+modalClosure.addEventListener("click", commandModal);
+// launch or close modal form
+// TODO #203 fonction commandant l'ouverture ou la fermeture de la modale
+function commandModal(event) {
+  if (event.currentTarget.className == "btn-signup modal-btn")
+    modalBackground.style.display = "block";
+  else if (event.currentTarget.className == "close") {
+    const modalContent = document.querySelector(".content");
+    // TODO #204 animer la fermeture de la modale
+    modalContent.animate([
+      {opacity: 1, transform: 'translateY(0px)', easing: 'ease-out'},
+      {opacity: 0, transform: 'translateY(-160px)'}
+    ], 1000
+    );
+    // attend la fin de l'animation pour faire disparaitre la modale
+    Promise.all(
+      modalContent.getAnimations().map(animation => animation.finished)
+    ).then(() => modalBackground.style.display = "none");
+  }
 }
-
-
